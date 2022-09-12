287a208f1e6ef214da37019b747b481fc4852e62
Christian Enigimi <eluifree4all@yahoo.com>
2022-09-12 14:08:00 -0700

    hhsh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,11 @@ int main(void)
 {
 	char lower = 'a';
 	char upper = 'A';
-	
 	while (lower <= 'z') /*print lowercases a-z*/
 	{
 		putchar(lower);
 		lower++;
 	}
-	
 	while (upper <= 'Z') /*print uppercase A-Z*/
 	{
 		putchar(upper);
