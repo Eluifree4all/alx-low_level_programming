f696a7193173ece64ec8dd2e018bd48c64324cd2
Christian Enigimi <eluifree4all@yahoo.com>
2022-09-13 21:25:43 -0700

    jsjjsj

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 	int row;
 	int column;
 	int product;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (row = 0; row <= n; row++)
@@ -19,25 +19,32 @@ void print_times_table(int n)
 			{
 				product = (row * column);
 				if (column == 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
 					_putchar('0' + product);
-				}
-				else if (product > 9 && product < 100)
+				else
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else if (product >= 100)
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{	
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}	
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
 				}
 			}
-		}
-		_putchar('\n');
+			_putchar('\n');
+		}	
 	}
-}
+}	
