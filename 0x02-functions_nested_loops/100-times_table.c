48f42bdaa155ec325e8cb571532570557c490fd5
Christian Enigimi <eluifree4all@yahoo.com>
2022-09-13 21:38:24 -0700

    sggfsgfgs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,6 +32,7 @@ void print_times_table(int n)
 					}
 					else if (product > 9 && product < 100)
 					{
+						_putchar(' ');
 						_putchar('0' + (product / 10));
 						_putchar('0' + (product % 10));
 					}
@@ -46,4 +47,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-}	
+}
