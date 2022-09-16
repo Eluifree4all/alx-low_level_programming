4183405f51895966416b48f67df4f837bf10da82
Christian Enigimi <eluifree4all@yahoo.com>
2022-09-16 15:26:18 -0700

    hjdhhdhddh

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,18 +7,10 @@
 
 int main(void)
 {
-	int n;
+	int i;
 	
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-		else if (n == 0)
-		{
-			printf("%i is zero\n", n);
-		}
-		else if (n < 0)
-		{
-			printf("%i is negative\n", n);
-		}
-		return (0);
+/*	i = 98;*/
+	positive_or_negative(i);
+	return (0);
+
 }
