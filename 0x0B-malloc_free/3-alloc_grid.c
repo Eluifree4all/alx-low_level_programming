84b75c00eb8a21902911ca7400bfd2d468592adf
Christian Enigimi <eluifree4all@yahoo.com>
2022-10-04 15:11:06 -0700

    5th commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -33,6 +33,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 		for (j = 0; j < width; j++) /* set array values to 0 */
-			grid[i][j] = 0;}
+			grid[i][j] = 0;
+	}
 	return (grid);
 }
