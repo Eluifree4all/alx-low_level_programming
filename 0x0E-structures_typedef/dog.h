000fdbf39450e9d1889ea1ec72603ae5b92eb6e8
Christian Enigimi <eluifree4all@yahoo.com>
2022-10-10 18:50:16 -0700

    moddded changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,13 +16,14 @@ typedef struct dog dog_t;
 */
 
 struct dog
+
 {
 	char *name;
 
 	float age;
 
 	char *owner;
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -31,5 +32,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
-
 #endif
