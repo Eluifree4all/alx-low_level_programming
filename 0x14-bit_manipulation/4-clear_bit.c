8b1138f437835b6464b4547e61b2268bc627634c
Christian Enigimi <eluifree4all@yahoo.com>
2022-10-27 18:42:19 -0700

    .

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -20,6 +20,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > max_bits)
 		return (-1);
 	/* create mask with 0 at index (...11011...) to work on that index */
+
 	mask = ~(mask << index);
 
 	*n = (*n & mask);
